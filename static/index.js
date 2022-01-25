84acd75635f5dcd57a181b659920a6732abe89c8
Deyi233 <98209978+Deyi233@users.noreply.github.com>
2022-01-25 14:33:43 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
-    return '人？';
+    if (score <= 49) return '给您拜年啦';
+    if (score <= 99) return '得得得得得';
+    if (score <= 149) return '新年快乐';
+    if (score <= 199) return '大哥抽黑蜘蛛';
+    return '祝您阖家欢乐';
 }
 
 function toStr(obj) {
